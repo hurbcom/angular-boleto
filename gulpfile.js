3d71011ab05e2d781638b81a2fd28bff575c3765
Bernardo Loureiro <bernardo.magalhaes@hotelurbano.com.br>
2015-04-22 16:30:42 -0300

    Gulpfile base

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp          = require('gulp');
+
+gulp.task('default', ['test'], function() {
+
+});
+
+gulp.task('test', function() {
+
+});
+
+gulp.task('dist', ['default'], function() {
+
+});
