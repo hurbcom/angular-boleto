f7eb3bfb15f490b1a09237f8dc937941330935d8
Bernardo Loureiro <bernardo.lou@gmail.com>
2019-10-01 12:24:45 -0300

    Updating gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,10 @@
-'use strict';
+const { src, dest, series, watch } = require('gulp');
+const browserSync = require('browser-sync');
+const wiredep = require('wiredep').stream;
+const inject = require('gulp-inject');
+const karma = require('karma').server;
 
-var gulp          = require('gulp');
-var browserSync   = require('browser-sync');
-var wiredep       = require('wiredep').stream;
-var inject        = require('gulp-inject');
-var karma         = require('karma').server;
-
-gulp.task('default', ['bower', 'inject'], function() {
-
-});
-
-// Levanta servidor pra dev
-gulp.task('serve', ['bower', 'inject', 'watch'], function() {
-
-});
-
-// Observa mudanças de arquivos e faz reload no browser sozinho
-gulp.task('watch', ['bower', 'inject'], function() {
+function watchTask() {
   browserSync({
     ui: false,
     port: 3005,
@@ -27,34 +15,34 @@ gulp.task('watch', ['bower', 'inject'], function() {
     }
   });
 
-  // Quando for o momento, criar watchs mais específicos
-  gulp.watch('app/**/*').on('change', browserSync.reload);
-});
+  watch(['app/**/*', '!app/bower_components/**/*']).on('change', browserSync.reload);
+};
 
-// Injeta as dependências do Bower no arquivo index.html
-gulp.task('bower', function() {
-  return gulp.src('app/index.html')
+function bower() {
+  return src('app/index.html')
     .pipe(wiredep({ optional: 'configuration', goes: 'here', devDependencies: true }))
-    .pipe(gulp.dest('app'));
-});
+    .pipe(dest('app'));
+};
 
-// Injeta os scripts e estilos da aplicação no arquivo index.html
-gulp.task('inject', ['bower'], function() {
-  // Ordem de injeção de scripts
-  var sources = gulp.src(
+function injectTask() {
+  var sources = src(
     ['./app/scripts/app.js',
-    '!./app/**/*.min.js',
-    './app/scripts/*.js'],
-    {read: false});
+      '!./app/**/*.min.js',
+      './app/scripts/*.js'],
+    { read: false });
 
-  return gulp.src('app/index.html')
+  return src('app/index.html')
     .pipe(inject(sources, { ignorePath: 'app' }))
-    .pipe(gulp.dest('app'));
-});
+    .pipe(dest('app'));
+};
 
-gulp.task('test', ['bower'], function (done) {
+function testTask() {
   karma.start({
     configFile: __dirname + '/karma.conf.js',
     singleRun: false
-  }, done);
-});
+  });
+};
+
+exports.default = series(bower, injectTask);
+exports.serve = series(bower, injectTask, watchTask);
+exports.test = series(bower, testTask);
